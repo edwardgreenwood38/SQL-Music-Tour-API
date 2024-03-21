6e55fc151fe7691e5c0fd9ebcfcd82d59a3891ca
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-03-21 17:25:12 -0400

    created migration for new column

---
+++ migrations/20240321211641-add-recommendation-column-to-bands.js
@@ -0,0 +1,22 @@
+'use strict';
+const { Sequelize } = require('sequelize');
+const { DataTypes } = require('sequleize');
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('bands', 'recommendation' {
+      type: DataTypes.string
+    })
+  },
+
+  down: async (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
