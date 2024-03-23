ed2b756d5590b874b33c7b85ff4f8de4f1cce4c1
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-03-23 18:54:45 -0400

    create band added

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -27,6 +27,21 @@ bands.get('/:id', async (req, res) => {
 })
 
 
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
 
 // EXPORT
 module.exports = bands
